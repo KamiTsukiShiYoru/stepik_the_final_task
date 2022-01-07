4c38fbf9be68895bf304428aa49a8231cf240bf7
Olga <sm_pochta_o@mail.ru>
2022-01-07 03:45:20 +0300

    Убрана параметризация test_guest_can_add_product_to_basket

--- test_product_page.py
+++ test_product_page.py
@@ -37,10 +37,8 @@ class TestUserAddToBasketFromProductPage:
 
 
 @pytest.mark.need_review
-@pytest.mark.parametrize('promo_number', ["0", "1", "2", "3", "4", "5", "6", pytest.param("7", marks=pytest.mark.xfail),
-                                          "8", "9"])
-def test_guest_can_add_product_to_basket(browser, promo_number):
-    link = f'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{promo_number}'
+def test_guest_can_add_product_to_basket(browser):
+    link = f'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer'
     page = ProductPage(browser, link)
     page.open()
     page.guest_can_add_product_to_basket()
